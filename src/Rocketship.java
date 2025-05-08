29d9fc944db6fa5cd437807c7a141d8d98d3739b
deanj <deanj@DeanPC>
2025-05-07 19:56:06 -0700

    I did 5 recipes

--- src/Rocketship.java
+++ src/Rocketship.java
@@ -1,7 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Rocketship extends GameObject{
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
 	public Rocketship(int x, int y, int width, int height) {
 		super(x,y,width,height);
 		speed = 10;
@@ -9,10 +15,19 @@ public class Rocketship extends GameObject{
 		x = Math.max(x, 500);
 		y = Math.min(y, 0);
 		y = Math.max(y, 800);
+		if (needImage) {
+		    loadImage ("rocket.png");
+		}
 	}
 	void draw(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect(x, y, width, height);
+		if (gotImage) {
+			g.drawImage(image, x, y, width, height, null);
+		} else {
+			g.setColor(Color.BLUE);
+			g.fillRect(x, y, width, height);
+		}
 	}
 	 public void right() {
 	        x+=speed;
@@ -26,5 +41,16 @@ public class Rocketship extends GameObject{
 	 public void down() {
 	       y+=speed;
 	 }
+	 void loadImage(String imageFile) {
+		    if (needImage) {
+		        try {
+		            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+			    gotImage = true;
+		        } catch (Exception e) {
+		            
+		        }
+		        needImage = false;
+		    }
+		}
 }
 
